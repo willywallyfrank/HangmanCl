e3f0ba642dff868b430c661ace7d89646a97c682
willywallyfrank <49914587+willywallyfrank@users.noreply.github.com>
2020-01-24 17:04:18 -0600

    Add files via upload

---
+++ HangMan_Design.py
@@ -0,0 +1,19 @@
+from gallows import *
+import hmGameClass
+
+
+def game_init():
+    pass
+
+def game_play(game):
+    pass
+
+def game_replay():
+    pass
+
+replay = True
+while replay:
+    game = game_init()
+    game_play(game)
+    game_replay()
+    
